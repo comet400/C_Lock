1d5e806c2458831ca830aec309283c477bb972c4
comet400 <Lukas.fvieira@hotmail.com>
2025-01-05 20:57:13 -0500

    Create Lexer Header file
    firstly we need a header file to declare all definitions and prototypes our lexer will use

---
+++ Headers/lexer.h
@@ -0,0 +1,13 @@
+/***********************************************************
+* File: lexer.h
+* This file contains the lexer for the interpreter.
+* The lexer is responsible for tokenizing the source code.
+* This Code was written by Lukas Fukuoka Vieira.
+* Contact: lukas.fvieira@hotmail.com
+* GitHub:https://github.com/comet400
+************************************************************/
+
+#pragma once
+
+#ifndef LEXER_H
+#define LEXER_H
